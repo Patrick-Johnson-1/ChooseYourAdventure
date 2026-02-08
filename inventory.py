cc1aa4384bf26083f038c7132d1c22265d6554b2
Patrick Clark Johnson <johnsonp8@nku.edu>
2026-02-08 16:47:50 -0500

    add_item method

--- inventory.py
+++ inventory.py
@@ -3,3 +3,9 @@ class Inventory:
         self.__items = []
         self.__max_size = max_size
 
+    def add_item(self, item):
+        if len(self.__items) < 10:
+            self.__items.append(self.__items)
+            return True
+        else:
+            return False
