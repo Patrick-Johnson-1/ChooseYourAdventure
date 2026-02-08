e5c0580a7cac65d933a58642c4242af17a65961a
Patrick Clark Johnson <johnsonp8@nku.edu>
2026-02-08 16:42:57 -0500

    Added Init

---
+++ player.py
@@ -0,0 +1,10 @@
+from character import Character
+
+class Player(Character):
+    def __init__(self, name):
+        super().__init__(name, 100, 15, 5)
+        self.__level = 1
+        self.__experience = 0
+        self.__gold = 0
+        self.__inventory = "PlaceHolder"
+
