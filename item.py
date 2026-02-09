c702dab0e03725db1d3a1e9ec5d4876a827d92b7
Patrick Clark Johnson <johnsonp8@nku.edu>
2026-02-09 12:34:06 -0500

    Potion class

--- item.py
+++ item.py
@@ -25,5 +25,19 @@ class Weapon(Item):
     def attack_bonus(self):
         return self.__attack_bonus
 
+class Potion(Item):
+    def __init__(self, name, description, value, heal_amount):
+        super().__init__(name,description,value)
+        self.__heal_amount = heal_amount
+    def __str__(self):
+        return f"{self.name} - ATK +{self.heal_amount} ({self.description})"
+    @property
+    def heal_amount(self):
+        return self.__heal_amount
+
+
 sword = Weapon("Iron Sword", "A sturdy blade", 50, 12)
 print(sword)  # "Iron Sword - ATK +12 (A sturdy blade)"
+
+potion = Potion("Health Potion", "Restores health", 15, 30)
+print(potion)  # "Health Potion - Heals 30 HP (Restores health)"
