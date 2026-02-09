8eec45a4c5c59915480ab0d9d3b41e54736f21ff
Patrick Clark Johnson <johnsonp8@nku.edu>
2026-02-09 12:53:40 -0500

    added more init

--- enemy.py
+++ enemy.py
@@ -3,4 +3,6 @@ from character import Character
 class Enemy(Character):
     def __init__(self, name, max_health,attack_power, defense, xp_reward, gold_reward):
         super().__init__(name, max_health, attack_power, defense)
+        self.__xp_reward = xp_reward
+        self.__gold_reward = gold_reward
 
