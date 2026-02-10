0f305ecf981bfe368e621252134bc73a1b2b3eca
Patrick Clark Johnson <johnsonp8@nku.edu>
2026-02-09 20:08:03 -0500

    AI assisted, finished off player_turn, save_game, and load_game

--- game.py
+++ game.py
@@ -62,10 +62,93 @@ class Game:
             message = f"You dealt {damage:.1f} damage!"
             return True, message
         elif action == "use_potion":
-            pass
-        elif action == "run":
-            pass
+            potions = self.__player.inventory.get_potions()
+            if len(potions) == 0:
+                return (False, "No potions available!")
 
+            potion = potions[0]  # Use first potion
+            self.__player.inventory.remove_item(potion.name)
+            self.__player.heal(potion.heal_amount)
+            return (True, f"Used {potion.name}! Healed {potion.heal_amount} HP")
+        elif action == "run":
+            if random.random() < 0.5:  # 50% chance
+                self.end_combat()
+                self.__in_combat = False
+                self.__current_enemy = None
+                return (True, "You escaped!")
+            else:
+                return (False, "Failed to escape!")
+
+    def save_game(self, filename):
+        """Save game state to JSON file"""
+        data = {
+            "player_name": self.__player.name,
+            "level": self.__player.level,
+            "experience": self.__player.experience,
+            "gold": self.__player.gold,
+            "current_health": self.__player.current_health,
+            "max_health": self.__player.max_health,
+            "attack_power": self.__player.attack_power,
+            "defense": self.__player.defense,
+            "inventory": []
+        }
+
+        # Save inventory items
+        for item in self.__player.inventory.get_items():
+            if isinstance(item, Weapon):
+                data["inventory"].append({
+                    "type": "weapon",
+                    "name": item.name,
+                    "description": item.description,
+                    "value": item.value,
+                    "attack_bonus": item.attack_bonus
+                })
+            elif isinstance(item, Potion):
+                data["inventory"].append({
+                    "type": "potion",
+                    "name": item.name,
+                    "description": item.description,
+                    "value": item.value,
+                    "heal_amount": item.heal_amount
+                })
+
+        with open(filename, 'w') as f:
+            json.dump(data, f, indent=2)
+
+    def load_game(self, filename):
+        """Load game state from JSON file"""
+        with open(filename, 'r') as f:
+            data = json.load(f)
+
+        # Create new player
+        self.__player = Player(data["player_name"])
+
+        # Restore stats using name mangling
+        self.__player._Player__level = data["level"]
+        self.__player._Player__experience = data["experience"]
+        self.__player._Player__gold = data["gold"]
+        self.__player._Character__current_health = data["current_health"]
+        self.__player._Character__max_health = data["max_health"]
+        self.__player._Character__attack_power = data["attack_power"]
+        self.__player._Character__defense = data["defense"]
+
+        # Restore inventory
+        for item_data in data["inventory"]:
+            if item_data["type"] == "weapon":
+                item = Weapon(
+                    item_data["name"],
+                    item_data["description"],
+                    item_data["value"],
+                    item_data["attack_bonus"]
+                )
+            elif item_data["type"] == "potion":
+                item = Potion(
+                    item_data["name"],
+                    item_data["description"],
+                    item_data["value"],
+                    item_data["heal_amount"]
+                )
+            self.__player.inventory.add_item(item)
     @staticmethod
     def create_random_enemy(player_level):
         rand = random.random()
